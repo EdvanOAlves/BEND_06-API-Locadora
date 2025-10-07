3bc77385639957cdeaa0cee0fce44bb21269bd32
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2025-10-07 11:09:03 -0300

    feat: adds message templates

--- controller/filme/controller_filme.js
+++ controller/filme/controller_filme.js
@@ -4,11 +4,56 @@
  * Data: 07/10/2025
  * Versão: 1.0.10.25
  **************************************************************************************************/
- 
- /**************************************************************************************************/
- //COMANDOS UTILIZADOS
- 
- 
- /**************************************************************************************************/
- //BIBLIOTECAS UTILIZADAS
- /**************************************************************************************************/
+
+/**************************************************************************************************/
+//COMANDOS UTILIZADOS
+
+
+/**************************************************************************************************/
+//BIBLIOTECAS UTILIZADAS
+/**************************************************************************************************/
+
+//Import da model do DAO de Filme
+const filmeDAO = require('../../model/DAO/filme.js');
+
+//Import do arquivo de mensagens
+const MESSAGES = require('../modulo/config_messages.js');
+
+// Retorna uma lista com todos os filmes
+const listarFilmes = async function () {
+    let resultFilmes = await filmeDAO.getSelectAllMovies();
+
+    if (resultFilmes){
+        if (resultFilmes.lenght > 0){
+            MESSAGES.MESSAGE_HEADER.status = MESSAGES.MESSAGE_REQUEST_SUCCESS.status; //Isso aqui é genial
+            MESSAGES.MESSAGE_HEADER.status_code = MESSAGES.MESSAGE_REQUEST_SUCCESS.status_code;
+            MESSAGES.MESSAGE_HEADER.items.filmes = resultFilmes;
+            return MESSAGES.MESSAGE_HEADER;
+        }
+    }
+
+}
+
+// Retorna um filme correspondente ao id inserido
+const buscarFilmeId = async function (id) {
+
+}
+
+// Insere um registro de filme no banco de dados
+const inserirFilme = async function(filme){
+
+}
+
+// Atualiza o registro de um filme correspondente ao id 
+const atualizarFilme = async function(filme, id){
+
+}
+
+// Exclui o registro de um filme correspondente ao id
+const excluirFilme = async function(id){
+
+}
+
+module.exports = {
+    listarFilmes
+}
---
+++ controller/modulo/config_messages.js
@@ -0,0 +1,36 @@
+/**************************************************************************************************
+ * Objetivo: Arquivo responsável pelos padrões de mensagens que o projeto irá realizar
+ *           Sempre no formato JSON (Mensagens de erro, sucesso, etc)
+ * Autor: Edvan Alves
+ * Data: 07/10/2025
+ * Versão: 1.0.10.25
+ **************************************************************************************************/
+
+//Armazenando a data da execução
+const today = Date.now();
+
+/*************************************** MENSAGENS PADRONIZADAS ***********************************/
+const MESSAGE_HEADER = {
+    development: 'Edvan Alves',
+    api_description: 'API para manipulação de dados de filmes',
+    status: Boolean,
+    status_code: Number,
+    request_date: today.getDate(),
+    items: {}
+}
+
+
+/*************************************** MENSAGENS DE SUCESSO *************************************/
+const MESSAGE_REQUEST_SUCCESS = {
+    status: true,
+    status_code: 200,
+    message: 'Requisição bem sucedida!'
+}
+
+
+/*************************************** MENSAGENS DE ERRO ****************************************/
+
+module.exports = {
+    MESSAGE_HEADER,
+    MESSAGE_REQUEST_SUCCESS
+}
